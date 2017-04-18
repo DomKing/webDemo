b4503bd7ff55a6ca9c74ed944781c472995d4fa4
kangduo <beixianchen@gmail.com>
2017-04-18 17:44:02 +0800

    优化权限决策器

--- src/main/java/org/prcode/security/WebSecurityConfig.java
+++ src/main/java/org/prcode/security/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public CustomerAccessDecisionManager customerAccessDecisionManager() {
-        return new CustomerAccessDecisionManager(securityService);
+        return new CustomerAccessDecisionManager();
     }
     @Bean
     public CustomerSecurityMetadataSource customerSecurityMetadataSource() {
